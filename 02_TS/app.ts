f296c0621e430c65888f102d421669f9dff6790c
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-01 17:12:16 +0900

    Add Obect Type

--- 02_TS/app.ts
+++ 02_TS/app.ts
@@ -1,18 +1,38 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-  //   throw new Error('入力ちが正しくありません');
-  //
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return n1 + n2;
+// function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+// if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+//   throw new Error('入力ちが正しくありません');
+//
+//   const result = n1 + n2;
+//   if (showResult) {
+//     console.log(phrase + result);
+//   } else {
+//     return n1 + n2;
+//   }
+// }
+
+// let number1 = 5;
+// const number2 = 2.8;
+// const printResult = true;
+// const resultPhrase = 'Result: ';
+
+// add(number1, number2, printResult, resultPhrase);
+
+// 明示的に型を指定するのはベストプラクティスではない
+const person = {
+  name: 'yuta',
+  age: 30,
+};
+
+console.log(person.name);
+
+// ネストしたObjectの型も定義できる
+// TypeScriptに型を推論を任せるのがベストプラクティス
+const product = {
+  id: 'abc1',
+  price: '12.99',
+  tags: ['great-offer', 'hot-and-new'],
+  details: {
+    title: 'Red Carpet',
+    description: 'A great carpet - almost brand-new!'
   }
 }
-
-let number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result: ';
-
-add(number1, number2, printResult, resultPhrase);
