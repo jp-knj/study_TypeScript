940582a0ce361da6860fec9dee3fde36a9db0c9f
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-13 16:10:26 +0900

    Add input some text to show it on console

--- 01_Taska-app/src/app.ts
+++ 01_Taska-app/src/app.ts
@@ -1,1 +1,54 @@
-// Code goes here!
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    // Inputの要素
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    mandayInputElement: HTMLInputElement;
+
+
+    constructor() {
+
+        // ! は指定された要素は必ず存在するとTypeScriptに教える
+        // as ????　は　型は　???? という割り当てをすること
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        // importNode関数でtemplateのcontent　を取得する
+        const importedNode = document.importNode(this.templateElement.content, true);
+
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+
+        // ElementIDを割り当てる'user-input';
+        this.element.id = 'user-input';
+
+        // Inputの要素
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.mandayInputElement = this.element.querySelector('#monday') as HTMLInputElement;
+
+
+        this.configure();
+        this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+
+
+    // 要素を追加する
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const prjInput = new ProjectInput();
