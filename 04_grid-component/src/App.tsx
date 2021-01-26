0a436e6259dd4300bb9b7b205c50607f2c21c804
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-26 23:50:02 +0900

    :+1: Add Grid components
    t

--- 04_grid-component/src/App.tsx
+++ 04_grid-component/src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Box from './components/Box/Box'
-
+import Grid from './components/Grid/Grid'
 
 function App() {
   return (
     <>
-    <Box>ボックス１</Box>
+      <Grid container>
+        <Grid item>
+          <Box>ボックス１</Box>
+        </Grid>
+        <Grid item>
+          <Box>ボックス2</Box>
+        </Grid>
+        <Grid item>
+          <Box>ボックス3</Box>
+        </Grid>
+      </Grid>
+
     </>
   );
 }
