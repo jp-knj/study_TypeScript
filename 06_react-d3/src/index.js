b8a405c903dd355e7972f2d8eeab79e13353ab9e
jp-knj <kenji.tomita1996@gmail.com>
2021-04-11 10:46:46 +0900

    :+1: Create generateData function
    t

--- 06_react-d3/src/index.js
+++ 06_react-d3/src/index.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import * as d3 from "d3";
 
 function App() {
+
+  // generate random data
+  // randomise data every time when click a btn.
+  const generateData = (value, length = 5) =>
+    d3.range(length).map((item, index) => ({
+      date: index,
+      value: value === null || value === undefined ? Math.random() * 100 : value
+    }));
+  const [data, setData] = useState(generateData());
+  const changeData = () => {
+    setData(generateData());
+  };
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div>
+        <button onClick={changeData}>Click</button>
+      </div>
     </div>
   );
 }
