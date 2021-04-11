fefbf82bdfd57296deff00b445e2c9c02c644173
jp-knj <kenji.tomita1996@gmail.com>
2021-04-11 11:23:44 +0900

    :+1: Add index.js
    t

--- 06_react-d3/src/index.js
+++ 06_react-d3/src/index.js
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import * as d3 from "d3";
 
+import PieClass from "./PieClass";
+import PieHooks from "./PieHooks";
+import PieSVG from "./PieSVG";
+
 function App() {
 
+  const [width, height, innerRadius, outerRadius] = [200, 200, 60, 100];
+
   // generate random data
-  // randomise data every time when click a btn.
+  // randomise data every time, When clicked a btn.
   const generateData = (value, length = 5) =>
     d3.range(length).map((item, index) => ({
       date: index,
@@ -20,6 +26,36 @@ function App() {
     <div className="App">
       <div>
         <button onClick={changeData}>Click</button>
+      </div>
+      <div>
+        <h2 className="label">React Class</h2>
+        <PieClass
+          data={data}
+          width={width}
+          height={height}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+        />
+      </div>
+      <div>
+        <h2>Hooks</h2>
+        <PieHooks
+          data={data}
+          width={width}
+          height={height}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+        />
+      </div>
+      <div>
+        <h2 className="label">SVG Elements</h2>
+        <PieSVG
+          data={data}
+          width={width}
+          height={height}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+        />
       </div>
     </div>
   );
