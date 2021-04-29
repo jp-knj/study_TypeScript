bbd46faaf7f1c3474dd04545a9d0815f104fae37
mono-tmt <tomita@monosus.co.jp>
2021-04-29 21:33:09 +0900

    :+1: using reducer
    t

--- 07_react-typescript/src/Application.tsx
+++ 07_react-typescript/src/Application.tsx
@@ -1,36 +1,43 @@
-import { ChangeEvent, useState } from 'react';
+import { useReducer } from 'react';
 
-const inc = (count: number) => count + 1;
-const dec = (count: number) => count - 1;
+type CounterAction = {
+  type: 'INCREMENT' | 'DECREMENT' | 'SET';
+  payload?: number;
+};
 
+type CounterState = {
+  value: number;
+};
+
+const reducer = (state: CounterState, action: CounterAction) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { value: state.value + 1 };
+    case 'DECREMENT':
+      return { value: state.value - 1 };
+    case 'SET':
+      return { value: action.payload || 0 };
+    default:
+      return state;
+  }
+};
 const Counter = () => {
- const [count, setCount] = useState(0);
- const changeCount = (event: ChangeEvent<HTMLInputElement>) => {
-  setCount(+event.target.value);
- };
- return (
-  <main className="Counter">
-   <h1>Day Since Last incidents</h1>
-   <p className="count">{count}</p>
-   <section className="controls">
-    <button onClick={() => setCount(inc)}>Increment</button>
-    <button onClick={() => setCount(0)}>Reset</button>
-    <button onClick={() => setCount(dec)}>Decrement</button>
-   </section>
-   <section className="controls">
-    <form onSubmit={() => {}}>
-     <label htmlFor="set-to">Set Count</label>
-     <input id="set-to" type="number" value={count} onChange={changeCount} />
-     <input
-      id="set-to"
-      type="number"
-      value={count}
-      onChange={(e) => setCount(+e.target.value)}
-     />
-    </form>
-   </section>
-  </main>
- );
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+  const increment = () => dispatch({ type: 'INCREMENT' });
+  const decrement = () => dispatch({ type: 'DECREMENT' });
+  const reset = () => dispatch({ type: 'SET', payload: 0 });
+  const set = (n: number) => dispatch({ type: 'SET', payload: n });
+  return (
+    <main className="Counter">
+      <h1>Day Since Last incidents</h1>
+      <p className="count">{state.value}</p>
+      <section className="controls">
+        <button onClick={increment}>Increment</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={decrement}>Decrement</button>
+      </section>
+    </main>
+  );
 };
 
 const Application = () => <Counter />;
