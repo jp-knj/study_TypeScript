7fec12ff152c04e16fcf36939f9945492ce3248e
mono-tmt <tomita@monosus.co.jp>
2021-04-29 10:43:46 +0900

    :+1: using react class
    t

--- 07_react-typescript/src/Application.tsx
+++ 07_react-typescript/src/Application.tsx
@@ -1,15 +1,57 @@
-import { Component } from 'react';
+import { ChangeEvent, Component } from 'react';
 
-class Counter extends Component {
+type CounterProps = {
+ incident: string;
+};
+type CounterState = {
+ count: number;
+};
+
+class Counter extends Component<CounterProps, CounterState> {
+ state: CounterState = {
+  count: 0
+ };
+
+ increment = () => {
+  this.setState(({ count }) => ({ count: count + 1 }));
+ };
+
+ decrement = () => {
+  this.setState(({ count }) => ({ count: count - 1 }));
+ };
+
+ reset = () => {
+  this.setState({ count: 0 });
+ };
+
+ changeCount = (event: ChangeEvent<HTMLInputElement>) => {
+  this.setState({ count: +event.target.value });
+ };
+
  render() {
+  const { incident } = this.props;
+  const { count } = this.state;
+
   return (
    <main className="Counter">
-    <h1>Day Since Last Incident</h1>
-    <p className="count">0</p>
+    <h1>Day Since Last {incident}</h1>
+    <p className="count">{count}</p>
     <section className="controls">
-     <button>Increment</button>
-     <button>Reset</button>
-     <button>Decrement</button>
+     <button onClick={this.increment}>Increment</button>
+     <button onClick={this.reset}>Reset</button>
+     <button onClick={this.decrement}>Decrement</button>
+    </section>
+    <section className="controls">
+     <form onSubmit={() => {}}>
+      <label htmlFor="set-to">Set Count</label>
+      <input
+       id="set-to"
+       type="number"
+       onChange={this.changeCount}
+       value={count}
+      />
+      <input type="submit" />
+     </form>
     </section>
    </main>
   );
@@ -18,7 +60,7 @@ class Counter extends Component {
 
 class Application extends Component {
  render() {
-  return <Counter />;
+  return <Counter incident="Coffee Split" />;
  }
 }
 export default Application;
