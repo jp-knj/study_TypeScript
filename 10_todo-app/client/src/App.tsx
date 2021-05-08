b987b183557501aa2825635bb92b94e460e94bff
jp-knj <kenji.tomita1996@gmail.com>
2021-05-08 21:59:51 +0900

    :+1: Add Components
    t

--- 10_todo-app/client/src/App.tsx
+++ 10_todo-app/client/src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import AddTodo from "./components/AddTodo";
+import { getTodos, addTodo } from "./API";
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  const fetchTodos = (): void => {
+    getTodos()
+    .then(({ data: { todos } }: ITodo[] | any) => setTodos(todos))
+    .catch((err: Error) => console.log(err))
+  }
+
+  const handleSaveTodo = (e: React.FormEvent, formData: ITodo): void => {
+  e.preventDefault()
+  console.log(formData);
+  addTodo(formData)
+    .then(({ status, data }) => {
+      if (status !== 201) {
+        throw new Error("Error! Todo not saved")
+      }
+      setTodos(data.todos)
+    })
+    .catch(err => console.log(err))
 }
+  return (
+    <main className="App">
+      <h1>My Todos</h1>
+      <AddTodo saveTodo={handleSaveTodo} />
+    </main>
+  );
+};
 
 export default App;
