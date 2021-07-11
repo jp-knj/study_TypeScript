c0313a916640f1546a7ad626492d75bab5766148
jp-knj <kenji.tomita1996@gmail.com>
2021-07-11 18:21:51 +0900

    :sparkles: Create pages to work for auth

---
+++ 14_react-firebase/src/pages/Home.tsx
@@ -0,0 +1,56 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+import { Button, Container, Grid, Typography } from "@material-ui/core";
+
+import auth from "../firebase";
+
+const Home = (props: any) => {
+  const [currentUser, setCurrentUser] = useState<null | object>(null);
+
+  useEffect(() => {
+    // if not logged in, redirect to login page
+    auth.onAuthStateChanged((user) => {
+      user ? setCurrentUser(user) : props.history.push("/login");
+    });
+  }, []);
+
+  return (
+    <Fragment>
+      <Container>
+        <Grid container style={{ marginTop: "1em" }}>
+          <Grid item md={4}></Grid>
+          <Grid item md={4}>
+            <Typography>Here is the user's information</Typography>
+            <Typography
+              variant="caption"
+              style={{
+                paddingTop: "2em",
+                paddingBottom: "2em",
+                whiteSpace: "pre",
+              }}
+            >
+              {currentUser && JSON.stringify(currentUser, null, 4)}
+            </Typography>
+            <Button
+              fullWidth
+              onClick={async (event) => {
+                try {
+                  await auth.signOut();
+                  props.history.push("/login");
+                } catch (error) {
+                  alert(error.message);
+                }
+              }}
+              style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+            >
+              Logout
+            </Button>
+          </Grid>
+          <Grid item md={4}></Grid>
+        </Grid>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default Home;
---
+++ 14_react-firebase/src/pages/Signup.tsx
@@ -0,0 +1,67 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+import {
+  Button,
+  Container,
+  FormControl,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+
+const Signup = (props: any) => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  useEffect(() => {
+    // if logged in, redirect to home
+  }, []);
+
+  return (
+    <Fragment>
+      <Container>
+        <Grid container>
+          <Grid item md={4}></Grid>
+          <Grid item md={4}>
+            <FormControl margin="normal" fullWidth>
+              <TextField
+                style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+                name="email"
+                label="E-mail"
+                fullWidth
+                variant="outlined"
+                value={email}
+              />
+            </FormControl>
+            <FormControl fullWidth>
+              <TextField
+                style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+                name="password"
+                label="Password"
+                fullWidth
+                variant="outlined"
+                type="password"
+                value={password}
+              />
+            </FormControl>
+            <FormControl fullWidth>
+              <Button
+                fullWidth
+                style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+              >
+                Sign up
+              </Button>
+              <Typography align="center">
+                <Link href="/login">to login</Link>
+              </Typography>
+            </FormControl>
+          </Grid>
+          <Grid item md={4}></Grid>
+        </Grid>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default Signup;
