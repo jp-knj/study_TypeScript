6a93d2b8913ae60033bb8a4d4c4c2f0e69ff3457
jp-knj <kenji.tomita1996@gmail.com>
2021-07-12 22:05:47 +0900

    :sparkles: Update SignUp Form

--- 15_react-firebase/src/pages/SignUp.tsx
+++ 15_react-firebase/src/pages/SignUp.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "../utils/firebase";
 
 const SignUp = () => {
-  return <h1>ユーザ登録</h1>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+  const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  };
+  const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  };
+  return (
+    <div>
+      <h1>ユーザ登録</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="email"
+            onChange={(event) => handleChangeEmail(event)}
+          />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input
+            name="password"
+            type="password"
+            onChange={(event) => handleChangePassword(event)}
+          />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
